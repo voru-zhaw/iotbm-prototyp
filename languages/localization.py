093b6e8998e51368fec7e596839fabfc70a897f9
voru-zhaw <95283935+voru-zhaw@users.noreply.github.com>
2021-11-30 13:49:49 +0100

    Update localization.py

--- languages/localization.py
+++ languages/localization.py
@@ -12,6 +12,6 @@ def get_language(code):
 
 # method to get the translation function
 def get_translation(page, code):
-    path = os.getcwd() + '\\languages'
+    path = os.getcwd() + '/languages'
     lang = gettext.translation(page, localedir=path, languages=[code])
     return lang.gettext
